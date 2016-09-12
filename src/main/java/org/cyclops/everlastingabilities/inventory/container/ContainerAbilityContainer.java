e98b9533627dd6cbddb83b8b4feeb2881f63a44f
Ruben Taelman <rubensworks@gmail.com>
2016-09-12 21:22:50 +0200

    Add safety for corrupted itemstacks while in inventory, Closes #18

--- src/main/java/org/cyclops/everlastingabilities/inventory/container/ContainerAbilityContainer.java
+++ src/main/java/org/cyclops/everlastingabilities/inventory/container/ContainerAbilityContainer.java
@@ -2,6 +2,7 @@ package org.cyclops.everlastingabilities.inventory.container;
 
 import com.google.common.collect.Lists;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
 import net.minecraftforge.fml.relauncher.Side;
 import net.minecraftforge.fml.relauncher.SideOnly;
 import org.cyclops.cyclopscore.helper.InventoryHelpers;
@@ -16,6 +17,7 @@ import org.cyclops.everlastingabilities.item.ItemAbilityTotem;
 import org.cyclops.everlastingabilities.item.ItemGuiAbilityContainer;
 
 import javax.annotation.Nullable;
+import java.util.Collections;
 import java.util.List;
 
 /**
@@ -64,15 +66,27 @@ public class ContainerAbilityContainer extends ItemInventoryContainer<ItemGuiAbi
     }
 
     public IMutableAbilityStore getItemAbilityStore() {
-        return getItemStack(player).getCapability(MutableAbilityStoreConfig.CAPABILITY, null);
+        ItemStack itemStack = getItemStack(player);
+        if (itemStack == null) {
+            return null;
+        }
+        return itemStack.getCapability(MutableAbilityStoreConfig.CAPABILITY, null);
     }
 
     public @Nullable List<Ability> getPlayerAbilities() {
-        return Lists.newArrayList(getPlayerAbilityStore().getAbilities());
+        IMutableAbilityStore abilityStore = getPlayerAbilityStore();
+        if (abilityStore == null) {
+            return Collections.emptyList();
+        }
+        return Lists.newArrayList(abilityStore.getAbilities());
     }
 
     public List<Ability> getItemAbilities() {
-        return Lists.newArrayList(getItemAbilityStore().getAbilities());
+        IMutableAbilityStore abilityStore = getItemAbilityStore();
+        if (abilityStore == null) {
+            return Collections.emptyList();
+        }
+        return Lists.newArrayList(abilityStore.getAbilities());
     }
 
     public void moveFromPlayer(Ability playerAbility) {
@@ -92,6 +106,6 @@ public class ContainerAbilityContainer extends ItemInventoryContainer<ItemGuiAbi
 
     @Override
     public boolean canInteractWith(EntityPlayer player) {
-        return getItem().canMoveFromPlayer() || (getItemStack(player) != null && !getItemAbilities().isEmpty());
+        return getItemStack(player) != null && (getItem().canMoveFromPlayer() || !getItemAbilities().isEmpty());
     }
 }
