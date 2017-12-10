8c37c98302f4f28b0ca1ac5f0d7f137ccf98e6a2
Ruben Taelman <rubensworks@gmail.com>
2017-12-10 18:02:46 +0100

    Make totem recycling more deterministic for different input orders

--- src/main/java/org/cyclops/everlastingabilities/recipe/TotemRecycleRecipe.java
+++ src/main/java/org/cyclops/everlastingabilities/recipe/TotemRecycleRecipe.java
@@ -8,10 +8,13 @@ import net.minecraft.util.NonNullList;
 import net.minecraft.world.World;
 import net.minecraftforge.common.ForgeHooks;
 import net.minecraftforge.registries.IForgeRegistryEntry;
+import org.cyclops.cyclopscore.helper.ItemStackHelpers;
 import org.cyclops.everlastingabilities.ability.AbilityHelpers;
 import org.cyclops.everlastingabilities.item.ItemAbilityTotem;
 import org.cyclops.everlastingabilities.item.ItemAbilityTotemConfig;
 
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Random;
 
 public class TotemRecycleRecipe extends IForgeRegistryEntry.Impl<IRecipe> implements IRecipe {
@@ -48,17 +51,14 @@ public class TotemRecycleRecipe extends IForgeRegistryEntry.Impl<IRecipe> implem
         int inputTargetIndex = rand.nextInt(ItemAbilityTotemConfig.totemCraftingCount);
         EnumRarity rarity = EnumRarity.COMMON;
 
+        // Sort our input stacks, so we can deterministically select one specific one to determine the output rarity.
+        NonNullList<ItemStack> sortedStacks = NonNullList.create();
         for (int i = 0; i < invCrafting.getSizeInventory(); i++) {
             ItemStack slot = invCrafting.getStackInSlot(i);
             if (!slot.isEmpty()) {
                 if (slot.getItem() instanceof ItemAbilityTotem) {
-                    if (inputIndex >= inputTargetIndex) {
-                        rarity = ItemAbilityTotem.getInstance().getRarity(slot);
-                        break;
-                    }
-                    inputIndex++;
-                }
-                else {
+                    sortedStacks.add(invCrafting.getStackInSlot(i));
+                } else {
                     // non-totem item found in recipe
                     // this should never happen because matches() will return false.
                     // We should probably throw() here.
@@ -66,9 +66,16 @@ public class TotemRecycleRecipe extends IForgeRegistryEntry.Impl<IRecipe> implem
                 }
             }
         }
+        Collections.sort(sortedStacks, Comparator.comparingInt(ItemStackHelpers::getItemStackHashCode));
+
+        if (inputTargetIndex >= sortedStacks.size()) {
+            // Should not be able to happen, unless some mod is doing funky stuff.
+            return ItemStack.EMPTY;
+        }
+        rarity = ItemAbilityTotem.getInstance().getRarity(sortedStacks.get(inputTargetIndex));
 
         // 20% chance of a bump
-        if (rarity.ordinal() < EnumRarity.EPIC.ordinal() && rand.nextInt(100) < ItemAbilityTotemConfig.totemCraftingRarityIncreasePercent) {
+        if (rand.nextInt(100) < ItemAbilityTotemConfig.totemCraftingRarityIncreasePercent && rarity.ordinal() < EnumRarity.EPIC.ordinal()) {
             rarity = EnumRarity.values()[rarity.ordinal()+1];
         }
 
